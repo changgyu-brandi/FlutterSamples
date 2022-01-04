3cf3791e425ec9838ba41ff2860770a9b080c9bc
changgyu <ghktks12!>
2022-01-04 15:50:06 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  GetStorage.init("UserInfo");
   runApp(const MyApp());
 }
 
@@ -11,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'GetX Storage',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'GetX Storage'),
     );
   }
 }
@@ -30,9 +32,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var myController = TextEditingController();
+  var storage = GetStorage();
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -40,13 +46,46 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            const Text(
-                'Master Branch'
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: TextField(
+                controller: myController,
+              ),
             ),
-            Text(
-              'Master Branch'
+            SizedBox(
+              height: 8,
             ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: ElevatedButton(
+                onPressed: () {
+                  if(GetUtils.isEmail(myController.text)){
+                    storage.write("email", myController.text);
+                  }else{
+                    Get.snackbar("Error", "이메일 형식이 맞지 않습니다");
+                  }
+                },
+                child: Text("Write"),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: ElevatedButton(
+                onPressed: () {
+                  print('The Stored Email : ${storage.read("email")}');
+                  // todo listener 삭제하는 방법 찾아보기
+                  // storage.listenKey("email", (value){
+                  //   print('email is change : ${value}');
+                  // });
+                },
+                child: Text("Read"),
+              ),
+            )
           ],
         ),
       ),
